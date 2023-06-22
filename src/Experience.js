7ef2932e71a4624b0df9cf475f602cf70d602f24
MANOJ KUMAR <100888762+Manojkumar1030@users.noreply.github.com>
2023-06-22 22:37:48 +0530

    first commit

--- src/Experience.js
+++ src/Experience.js
@@ -1,4 +1,4 @@
-import { Center, OrbitControls, Sparkles, shaderMaterial, useGLTF, useTexture } from '@react-three/drei'
+import { Center, OrbitControls, Stars,Sparkles, shaderMaterial, useGLTF, useTexture } from '@react-three/drei'
 import portalFragmentShader from "./shaders/portal/fragment.js"
 import portalVertexShader from "./shaders/portal/vertex.js"
 import * as THREE from "three"
@@ -10,11 +10,12 @@ import { useControls } from 'leva'
 export default function Experience()
 {
 
-
+    const colorStart = useControls({startColor:"#ffffff"})
+    const colorEnd = useControls({endcolor:"#000000"})
 
     const PortalMaterial = shaderMaterial({
         uTime:0,
-        uColorStart:new THREE.Color("#ffffff"),
+        uColorStart:new THREE.Color(`#ffffff`),
         uColorEnd:new THREE.Color(`#000000`)
     },
     portalVertexShader,
@@ -27,6 +28,8 @@ export default function Experience()
     const portalref=useRef()
     useFrame((state,delta)=>{
         portalref.current.uTime += delta*1.25
+        portalref.current.uColorStart=new THREE.Color(`${colorStart.startColor}`)
+        portalref.current.uColorEnd=new THREE.Color(`${colorEnd.endcolor}`)
     })
 
     const {nodes} =useGLTF("./model/finalportal.glb")
@@ -36,12 +39,10 @@ export default function Experience()
     return <>
     <Perf position='top-left' />
 
-        <Center>
-            
             <OrbitControls makeDefault />
             
             {/* Portal */}
-
+            <Center>
             <mesh geometry={nodes.baked.geometry} >
                 <meshBasicMaterial map={bakedTexture} />
             </mesh>
@@ -59,7 +60,7 @@ export default function Experience()
                 geometry={nodes.poleLightB.geometry}
                 position={nodes.poleLightB.position}
                  >
-                <meshBasicMaterial color="#ffffe5"/>
+                <meshBasicMaterial color="#ffffe5" />
             </mesh>
 
             {/* Portal Light */}
@@ -69,28 +70,18 @@ export default function Experience()
                 position={nodes.portalLight.position}
                 rotation={nodes.portalLight.rotation}
             >
-                {/* <shaderMaterial 
-                        vertexShader={portalVertexShader}
-                        fragmentShader={portalFragmentShader}  
-                        uniforms={{
-                            uTime:{value:0},
-                            uColorStart:{value:new THREE.Color("#ffffff") },
-                            uColorEnd:{value:new THREE.Color("#000000") }
-
-                        }}                  
-                    /> */}
-                    <portalMaterial ref={portalref}/>
+                    <portalMaterial ref={portalref} />
             </mesh>
             
             {/* Fireflies */}
             <Sparkles 
                 size={6}
                 scale={[4,2,4]}
-                count={65}
+                count={60}
                 speed={0.5}
-                position-y={1}
+                position-y={1.2}
             />
-
+            {/* <Stars radius={10} depth={50} count={5000} factor={4} saturation={0} fade speed={1} /> */}
         </Center>
     </>
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.js'
+import { Stage } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -15,6 +16,9 @@ root.render(
             position: [ 1, 2, 6 ]
         } }
     >
+    
+
         <Experience />
+
     </Canvas>
 )
